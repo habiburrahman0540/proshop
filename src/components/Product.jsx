7b9a55c4b360a368b54663fd680d1a717506dcbd
unknown <habiburrahman0540@gmail.com>
2021-01-04 16:09:34 +0400

    Rating component added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,6 @@
 
 import {Card} from 'react-bootstrap'
+import Rating from './Rating'
 const Product = ({product})=>{
     return(
         <Card className="my-3 p-3 rounded">
@@ -11,9 +12,7 @@ const Product = ({product})=>{
                             <Card.Title as="div"><strong>{product.name}</strong></Card.Title>
                     </a>
                                 <Card.Text as="div">
-                                        <div className="py-3"> 
-                                                {product.rating} from {product.numReviews} reviews
-                                        </div>
+                                       <Rating value={product.rating} text={`${product.numReviews} review`}/>
                                 </Card.Text>
                                 <Card.Text as="h3">
                                         $ {product.price}
